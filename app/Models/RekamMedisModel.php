69d4c4ef4433609ce8434818580dae2b1b2869c4
sitisulaiha24 <137159764+sitisulaiha24@users.noreply.github.com>
2023-08-03 05:06:58 +0700

    gug

--- app/Models/RekamMedisModel.php
+++ app/Models/RekamMedisModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class RekamMedisModel extends Model
 {
     protected $DBGroup          = 'default';
-    protected $table            = 'RekamMedis';
+    protected $table            = 'rekammedis';
     protected $primaryKey       = 'id_rekam_medis';
     protected $useAutoIncrement = false;
     protected $allowedFields    = ['id_rekam_medis', 'tanggal', 'keluhan', 'pemeriksaan', 'pengobatan'];
